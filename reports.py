fa72bffc12222ae7244d19fbd77615a8403f5ece
Mev1s <104010839+Mev1s@users.noreply.github.com>
2026-02-21 17:54:57 +0700

    feat: reports.py new loop to convert List[Dict[str, list]] data to List[List[int, str, float]]

--- reports.py
+++ reports.py
@@ -1,15 +1,25 @@
 from csv_reader import csv_reader
 from typing import Dict, List
 
-def average_gdp_function(data: List[Dict[str, str]]) -> Dict[str, list[str]]:
-    countries_gdp = {}
+def average_gdp_function(
+        data: List[Dict[str, str]]
+                         ) -> List[list[int, str, float]]:
 
-    for row in data: # создаем словарь с парами страна - list(ВВП)
-        if row['country'] not in countries_gdp:
-            countries_gdp[row['country']] = [row['gdp']]
-        else:
-            countries_gdp[row['country']].append(row['gdp'])
-    return countries_gdp
+    countries_gdp = {}
+    result_out = []
+
+    for row in data:
+        if row['country'] not in countries_gdp:  # создаем словарь с парами страна - list(ВВП)
+            countries_gdp[row['country']] = [int(row['gdp'])]
+        else: # если уже есть, то в list этой страны добавляем значение
+            countries_gdp[row['country']].append(int(row['gdp']))
+
+    for ind, row in enumerate(countries_gdp.items()): # приводим
+        result_out.append(
+            [ind+1, row[0], round(sum(row[1]) / len(row[1]), 2)]
+        )
+
+    return result_out
 
 
 REPORTS = {"average-gdp": average_gdp_function} # введеный аргумент (--report) = нужная функция
