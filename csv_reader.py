2b2172687bd8fe770db8033cf8410ada3ae00a10
Mev1s <104010839+Mev1s@users.noreply.github.com>
2026-02-21 16:29:36 +0700

    feat

---
+++ csv_reader.py
@@ -0,0 +1,19 @@
+import csv
+
+from parser import parse_args
+
+args = parse_args()
+
+def csv_reader():
+    data = []
+    for file in args.files:
+        try:
+            with open(file) as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    data.append(row)
+        except FileNotFoundError:
+            print(f"{file} не найден")
+            return "file not found"
+    print(data)
+    return data
